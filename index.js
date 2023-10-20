78ae0a30c5e7a01f02ba7cd6c93c3beaab1af324
Batur Akkurt <halidbaturakkurt@gmail.com>
2023-10-20 23:15:50 +0300

    feat: add static contents

--- index.js
+++ index.js
@@ -1,16 +1,42 @@
 #!/usr/bin/env node
 
 import figlet from "figlet";
+import prompts from "prompts";
+import chalk from "chalk";
+import chalkAnimation from "chalk-animation";
 
-figlet.text(
-  "Batur",
-  {
-    font: "Cyberlarge",
-  },
-  (err, data) => {
-    if (err) {
-      throw err;
-    }
-    console.log(data);
-  }
-);
+function renderHeader(title = "Batur") {
+  return console.log(
+    chalk.hex("#E4111F").bold(
+      figlet.textSync(title, {
+        font: "Cyberlarge",
+      })
+    )
+  );
+}
+
+async function renderDescription() {
+  return console.log(`${chalk.bold("Hi")}, I'm Batur 👋
+Just developer, software and other things.
+
+${chalk.italic("aka Pax Tartarica")}
+${chalk.italic("Currently working at SHFT as")} ${chalk.bold("Tech Lead")}
+  `);
+}
+
+function renderFooter() {
+  return console.log(chalk.bold.italic.gray("NEC SPE NEC METU"), "\n\n");
+}
+
+function renderPrompts() {
+  return;
+}
+
+function main() {
+  renderHeader();
+  renderDescription();
+  renderFooter();
+  renderPrompts();
+}
+
+main();
