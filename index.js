680c22bfe79ee5a791634ff8c21f9fe97959ce78
Batur Akkurt <halidbaturakkurt@gmail.com>
2023-10-21 11:47:02 +0300

    feat: add prompts

--- index.js
+++ index.js
@@ -3,9 +3,9 @@
 import figlet from "figlet";
 import prompts from "prompts";
 import chalk from "chalk";
-import chalkAnimation from "chalk-animation";
+import open from "open";
 
-function renderHeader(title = "Batur") {
+function renderHeader(title = "Batur 👋") {
   return console.log(
     chalk.hex("#E4111F").bold(
       figlet.textSync(title, {
@@ -16,8 +16,7 @@ function renderHeader(title = "Batur") {
 }
 
 async function renderDescription() {
-  return console.log(`${chalk.bold("Hi")}, I'm Batur 👋
-Just developer, software and other things.
+  return console.log(`Just developer, software and other things.
 
 ${chalk.italic("aka Pax Tartarica")}
 ${chalk.italic("Currently working at SHFT as")} ${chalk.bold("Tech Lead")}
@@ -25,11 +24,38 @@ ${chalk.italic("Currently working at SHFT as")} ${chalk.bold("Tech Lead")}
 }
 
 function renderFooter() {
-  return console.log(chalk.bold.italic.gray("NEC SPE NEC METU"), "\n\n");
+  return console.log(chalk.bold.italic.gray("NEC SPE NEC METU"), "\n");
 }
 
 function renderPrompts() {
-  return;
+  return prompts({
+    type: "select",
+    name: "value",
+    message: "What do you want to do?",
+    choices: [
+      { title: "🌐 Website", value: "https://www.batur.io/" },
+      {
+        title: "📃 Curriculum Vitae",
+        value: "https://linkedin.com/in/ta2ulk/",
+      },
+      {
+        title: "💻 GitHub",
+        value: "https://github.com/batur",
+      },
+      {
+        title: "✖️  Twitter",
+        value: "https://twitter.com/ta2ulk",
+      },
+      {
+        title: "📓 Medium",
+        value: "https://medium.com/@ta2ulk",
+      },
+    ],
+  }).then((response) => {
+    if (response.value) {
+      return open(response.value);
+    }
+  });
 }
 
 function main() {
